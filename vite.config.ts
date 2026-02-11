e9e36aa10adaf165fbb1dabda8562a31eb8d73ba
susumuuenoyama-dotcom <susumu.uenoyama@gmail.com>
2026-02-11 18:42:34 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/zuka-putter-game/', // この一行が、GitHub Pagesで芝生を映す鍵です
 })
