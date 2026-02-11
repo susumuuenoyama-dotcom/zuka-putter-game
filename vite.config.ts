6b7bf284c73ebf516db60ed960769cde7cee12dc
susumuuenoyama-dotcom <susumu.uenoyama@gmail.com>
2026-02-11 12:32:45 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // リポジトリ名に合わせて base を設定（例: /zuka-putter-sim-pro/）
-  // ユーザー名.github.io の直下に置く場合は '/' でOKです
-  base: './', 
-  build: {
-    outDir: 'dist',
-  }
-});
+  base: './',
+  plugins: [react()],
+})
